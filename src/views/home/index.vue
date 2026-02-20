507b397d57c333ef55363dc64c2bad928cc6be92
jarvenwang <wang_jin_bao@163.com>
2026-02-20 18:43:43 +0800

    style(home-jackpot): slightly increase right offset of left icon

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1475,7 +1475,7 @@ onUnmounted(() => {
         height: 100%;
         object-fit: contain;
         display: block;
-        transform: translateX(10px) scale(1.14);
+        transform: translateX(12px) scale(1.14);
       }
     }
 
